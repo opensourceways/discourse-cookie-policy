2b3d10fdcb150797e159bfd1541ada3ad931a124
Pete <peterfrench94@gmail.com>
2024-12-05 15:56:20 +0100

    refactor: Setup init.js

---
+++ javascripts/discourse/cookie-policy/main.js
@@ -0,0 +1,65 @@
+import { Notification } from "./notification.js";
+import { Manager } from "./manager.js";
+import {
+  preferenceNotSelected,
+  hideSpecified,
+  addGoogleConsentMode,
+  loadConsentFromCookie,
+} from "./utils.js";
+
+// Add Google Consent Mode as soon as the script is loaded
+addGoogleConsentMode();
+
+export const cookiePolicy = (callback = null) => {
+  let cookiePolicyContainer = null;
+  let language = document.documentElement.lang;
+
+  const renderNotification = function (e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    if (cookiePolicyContainer === null) {
+      cookiePolicyContainer = document.createElement("dialog");
+      cookiePolicyContainer.classList.add("cookie-policy");
+      cookiePolicyContainer.setAttribute("open", true);
+      document.body.appendChild(cookiePolicyContainer);
+      const notifiation = new Notification(
+        cookiePolicyContainer,
+        renderManager,
+        close
+      );
+      notifiation.render(language);
+      document.getElementById("cookie-policy-button-accept").focus();
+    }
+  };
+
+  const renderManager = function () {
+    const manager = new Manager(cookiePolicyContainer, close);
+    manager.render(language);
+  };
+
+  const close = function () {
+    if (typeof callback === "function") {
+      callback();
+    }
+    document.body.removeChild(cookiePolicyContainer);
+    cookiePolicyContainer = null;
+  };
+
+  const init = function () {
+    // Load the consent from the cookie, if available
+    loadConsentFromCookie();
+
+    const revokeButton = document.querySelector(".js-revoke-cookie-manager");
+    if (revokeButton) {
+      revokeButton.addEventListener("click", renderNotification);
+    }
+
+    if (preferenceNotSelected() && !hideSpecified()) {
+      renderNotification();
+    }
+  };
+
+  document.addEventListener("DOMContentLoaded", init, false);
+};
