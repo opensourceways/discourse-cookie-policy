6d6dba2fbbcc94dab35ebdcb76c412fa243d0d6e
Pete <peterfrench94@gmail.com>
2024-12-11 14:24:45 +0100

    chore: Upgrade cookie-policy to 3.6.5

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -8,9 +8,4 @@ export default apiInitializer("1.8.0", (/* api */) => {
 async function loadCookiePolicy() {
   await loadScript(settings.theme_uploads_local.cookie_policy_js);
   cpNs.cookiePolicy();
-  // Components are loaded after DOMContentLoaded event, so we need to dispatch it manually. We should replace this with a custom event in the future.
-  document.dispatchEvent(new Event("DOMContentLoaded", {
-    bubbles: true,
-    cancelable: true
-  }));
 }
